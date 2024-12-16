e587080039e8063f5aafb235e34d857d66be5fb2
2cdn2219 <2cdn2219@mail.u-tokai.ac.jp>
2024-12-16 15:45:53 +0900

    Squashed commit of the following:
    commit 0e8b1b93a87d30ee53745e428b9ffe2ac7d22e99
    Merge: 46b3dc4 dac1ecb
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Mon Dec 16 15:40:07 2024 +0900
    
        Merge pull request #14 from ibuki1124/feature-2cdn2222
    
        Feature 2cdn2222
    
    commit dac1ecba87a8b4a556a87ff97f6db2a835992412
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Mon Dec 16 15:39:29 2024 +0900
    
        [Delete] 不要なファイル
    
    commit 32f175fcd54ad9bdb2307d4c258067f5eee24885
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Mon Dec 16 15:38:18 2024 +0900
    
        [Delete] 不要なファイル
    
    commit b955f00e7621d9a147bc92d8ecf33a4888df55f9
    Merge: 76440a1 46b3dc4
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Mon Dec 16 15:34:32 2024 +0900
    
        Merge branch 'main' of github.com:ibuki1124/labB into feature-2cdn2222
    
    commit 76440a1367bc884b833ac96b70cead7af4f1385f
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Mon Dec 16 15:30:59 2024 +0900
    
        [Add] cssファイル
    
    commit 46b3dc4e0480b27b16405896699985a39a24af9d
    Merge: 79e3084 9ab7e35
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Mon Dec 16 15:24:28 2024 +0900
    
        Merge pull request #11 from ibuki1124/login-success_css
    
        login-success
    
    commit 79e30845e1fb41da9ae15244e96db329902e54f5
    Merge: eea120b 9aa3b77
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Mon Dec 16 15:24:12 2024 +0900
    
        Merge pull request #9 from ibuki1124/login-css
    
        Login css
    
    commit eea120b8173ede37012bc894afd7a681f19a2d22
    Merge: 43c5d76 3b1ef22
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Mon Dec 16 15:23:41 2024 +0900
    
        Merge pull request #8 from ibuki1124/top-css
    
        top-css
    
    commit 9ab7e35c02a468e6e3f44f3171ffc183c5795d76
    Author: 2cdn2108 <2cdn2108@mail.u-tokai.ac.jp>
    Date:   Mon Dec 16 01:04:49 2024 +0900
    
        login-success
    
    commit 9aa3b77c69b3d4fb692ca8ce4893002f5c22b53d
    Author: 2cdn2108 <2cdn2108@mail.u-tokai.ac.jp>
    Date:   Tue Dec 10 13:37:01 2024 +0900
    
        login-css2
    
    commit 68562e3c50cc1ad777d6b31ca15e6e6630b3a2db
    Author: 2cdn2108 <2cdn2108@mail.u-tokai.ac.jp>
    Date:   Tue Dec 10 13:31:40 2024 +0900
    
        login-css
    
    commit 3b1ef2225fe5bd76b335c379ed49064a705773b9
    Author: 2cdn2108 <2cdn2108@mail.u-tokai.ac.jp>
    Date:   Tue Dec 10 12:44:49 2024 +0900
    
        top-css

--- login.php
+++ login.php
@@ -2,15 +2,18 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" href="login.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ログインページ</title>
 </head>
+<body>
 <div class="container">
     <form>
-        <p class="fsize">ログイン画面</p>
+        <p class="fsize">災害対策アプリにログイン</p>
         <input type="text" placeholder="Username" name="username" />
         <input type="password" placeholder="Password" name="password" />
         <button type="button" onclick="location.href='login-success.html'">ログイン</button>
     </form>
 </div>
+</body>
 </html>
