76440a1367bc884b833ac96b70cead7af4f1385f
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-12-16 15:30:59 +0900

    [Add] cssファイル

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="login.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ログインページ</title>
+</head>
+<body>
+<div class="container">
+    <form>
+        <p class="fsize">災害対策アプリにログイン</p>
+        <input type="text" placeholder="Username" name="username" />
+        <input type="password" placeholder="Password" name="password" />
+        <button type="button" onclick="location.href='login-success.html'">ログイン</button>
+    </form>
+</div>
+</body>
+</html>
---
+++ top.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="top.css">
+    <title>災害対策アプリ</title>
+</head>
+<body>
+    <header>
+        <img src="C:\xampp\htdocs\labB\アンケートシートのフリー素材.png">
+        <div class="aplication_title">災害対策アプリ</div>
+        <nav class="nav-links">
+            <a href="login.html">ログインページへ</a>
+            <a href="#mypage">マイページへ</a>
+            <a href="#history">過去の結果へ</a>
+        </nav>
+    </header>
+
+    <h1 class="aplication_name">災害対策アプリ</h1>
+
+    <div class="container">
+        <a href="#check" class="button">対策チェックへ</a>
+        <a href="#actions" class="button">災害時の行動(リスト、災害別)へ</a>
+    </div>
+</body>
+</html>
