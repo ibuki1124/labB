80828a0c8dc17099d8be7fe9f694459f4842d721
2cdn2219 <2cdn2219@mail.u-tokai.ac.jp>
2024-12-17 16:50:13 +0900

    Squashed commit of the following:
    commit 249c805a101693ddc4dd00d76a833fc228180400
    Merge: 7294d64 167ab72
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Tue Dec 17 16:47:00 2024 +0900
    
        Merge pull request #26 from ibuki1124/feature-2cdn2222
    
        Feature 2cdn2222
    
    commit 167ab722d489188c22ab7cd58b0d10e5979b31f6
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Tue Dec 17 16:46:04 2024 +0900
    
        [Add] データベース接続テンプレートファイル
    
    commit 7294d6482486a2ea14a4e98bc52d491389c4d384
    Merge: 995645c 042ed23
    Author: 2CDN1122 <147790468+2CDN1122@users.noreply.github.com>
    Date:   Tue Dec 17 16:37:37 2024 +0900
    
        Merge pull request #25 from ibuki1124/2cdn1122
    
        header
    
    commit 042ed23f9f5f28d31a0918df7389e1f2ac2e55b0
    Author: 2CDN1122 <2cdn1122@gmail.com>
    Date:   Tue Dec 17 16:37:01 2024 +0900
    
        header
    
    commit 995645ca1ee8635b03722e89f97fa80400da889d
    Merge: 92b2bb1 ecc66b9
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Tue Dec 17 16:15:56 2024 +0900
    
        Merge pull request #24 from ibuki1124/top_mypage
    
        mypage
    
    commit ecc66b9bd2d5367fe554f9533ed6bceb531c7f6d
    Author: 2cdn2108 <2cdn2108@mail.u-tokai.ac.jp>
    Date:   Tue Dec 17 15:59:11 2024 +0900
    
        mypage
    
    commit 92b2bb199b09ac9bb29f62ab5aff7091ace3ea5a
    Merge: 5b60ba3 032df9e
    Author: 2CDN1122 <147790468+2CDN1122@users.noreply.github.com>
    Date:   Tue Dec 17 14:31:14 2024 +0900
    
        Merge pull request #23 from ibuki1124/2cdn1122
    
        php,css
    
    commit 032df9e7c1b718ae3fa581f33f5fd1a3c7ef2d1d
    Author: 2CDN1122 <2cdn1122@gmail.com>
    Date:   Tue Dec 17 14:30:07 2024 +0900
    
        php,css
    
    commit d791d9ec84cf492393cb8687910100e8a57ab512
    Merge: f5f762a 5b60ba3
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Tue Dec 17 14:10:12 2024 +0900
    
        Merge branch 'main' of github.com:ibuki1123/labB into feature-2cdn2222
    
    commit f5f762a15aead1619d063e3001861bf85e912da1
    Merge: 5155273 a49a6b7
    Author: ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
    Date:   Tue Dec 17 12:28:44 2024 +0900
    
        Merge branch 'main' of github.com:ibuki1123/labB into feature-2cdn2222

---
+++ php/temp/db.php
@@ -0,0 +1,18 @@
+<?php
+  require __DIR__ . '/../../vendor/autoload.php';
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
+  $dotenv->load();
+  $db = "mysql:dbname={$_ENV['DBNAME']};host={$_ENV['HOST']}";
+  $user = $_ENV["USER"];
+  $pass = $_ENV["PASS"];
+  $pdo = new PDO($db, $user, $pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
+  // 表示されているか確認したい場合にコメントアウト外す
+  // $sql = "SELECT * FROM users";
+  // $stmt = $pdo->prepare($sql);
+  // $stmt->execute();
+  // $results = $stmt->fetchAll();
+  // foreach ($results as $row){
+  //   echo $row["name"]."<br>";
+  // }
+?>
