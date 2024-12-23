e20daaac89ad196481a28c3ee514caa59cf5e1c8
FunadaAyari <2cdn1231@mail.u-tokai.ac.jp>
2024-12-23 12:18:28 +0900

    Update result.php, pastresulte.php
    スコア表示機能の追加

--- php/result.php
+++ php/result.php
@@ -16,29 +16,49 @@
     <body>
         <?php
             session_start();
+            $user_id = $_SESSION["user_id"];
+
+            include("temp/db.php");
+            $sql = "SELECT * FROM past_scores WHERE user_id = :user_id ORDER BY date DESC LIMIT 1;";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+            $stmt->execute();
+            $latest_result = $stmt->fetch();
+        
+            $sql = "SELECT id, action FROM action_lists";
+            $stmt = $pdo -> prepare($sql);
+            $stmt->execute();
+            $actions = $stmt->fetchAll();
+
             include("temp/header.php");
         ?>
         <main class="align-center main">
-            <div class="main">
-                <h2 class="page-title">災害対策チェック結果</h2>
-                <p class="">日付</p>
-            </div>
+            <?php if (!empty($latest_result)): ?>
+                <div class="main">
+                    <h2 class="page-title">災害対策チェック結果</h2>
+                    <p class="">日付：<?= $latest_result['date']; ?></p>
+                </div>
                 <div class="main-topc align-center">
-                    <h1>スコア：〇</h1>
+                    <h1>スコア：<?= $latest_result['score']; ?></h1>
                 </div>
                 <table class="stocktable align-center">
                     <thead>
                         <tr><th>対策</th><th>結果</th></tr>
                     </thead>
                     <tbody>
-                    <!--繰り返しで表作成-->
-                    <tr><td>対策</td><td>〇×</td></tr>
+                    <?php foreach ($actions as $a): ?>
+                        <tr><td><?= $a['action'] ?></td>
+                        <td>
+                            <?php if (in_array($a['id'], json_decode($latest_result['check_index'], true))): ?>〇
+                            <?php else: ?>×
+                            <?php endif; ?>
+                        </td></tr>
+                    <?php endforeach; ?>    
                     </tbody>
                 </table>
-                <a href="top.php" class="btn"name="">トップ画面に戻る</a>
-                
-                
             </div>
+            <?php endif; ?>
+            <a href="top.php" class="btn"name="">トップ画面に戻る</a>
         </main>
     </body>
 </html>
