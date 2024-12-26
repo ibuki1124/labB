67ee59b429161ea0703f8f837a96787e17714830
2cdn2108 <2cdn2108@mail.u-tokai.ac.jp>
2024-12-26 21:59:17 +0900

    resetcss

--- php/login-success.php
+++ php/login-success.php
@@ -2,6 +2,9 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
+  <!--リセットCSS-->
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/login-success.css">
   <link rel="stylesheet" href="../css/header.css">
--- php/mypage.php
+++ php/mypage.php
@@ -2,6 +2,8 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <!--リセットCSS-->
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
     <link rel="stylesheet" href="../css/mypage.css">
     <link rel="stylesheet" href="../css/header.css">
     <meta name="viewport" content="width=device-width,initial-scale=1">
