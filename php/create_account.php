c9bfaf32568934f0829aaa1a24b8d6bb4047e01e
2CDN1122 <2cdn1122@gmail.com>
2024-12-27 23:25:07 +0900

    css

--- php/create_account.php
+++ php/create_account.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
   <link rel="stylesheet" href="../css/create_account.css">
   <link rel="stylesheet" href="../css/header.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
